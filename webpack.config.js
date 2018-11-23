68c965dbc98033705f9ed31358720c276c9f498c
faisal <faisalhmohd@gmail.com>
2018-11-24 01:27:49 +0600

    Add cleaning of dist and generating HTML

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,4 +8,28 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'The Platform',
+    })
+  ],
+  module: {
+    rules: [
+      // Adds 'import .png/svg/jpg/gif' support
+      {
+	test: /\.(png|svg|jpg|gif)$/,
+	use: [
+	  'file-loader'
+	]
+      },
+      // Adds 'import fontfile' support 
+      {
+	test: /\.(woff|woff2|eot|ttf|otf)$/,
+	use: [
+	  'file-loader'
+	]
+      }
+    ]
+  }
 };
