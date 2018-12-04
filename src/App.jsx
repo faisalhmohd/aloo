c270a55d236f1f35db8109fe92d7038a9b7949ea
faisal <faisalhmohd@gmail.com>
2018-12-04 13:51:50 +0600

    Add Routers

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,24 @@ import React from 'react';
 import Header from './Header';
 import Body from './Body';
 import Footer from './Footer';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+function Test() {
+  return (
+    <div>Hi!</div>
+  );
+}
 
 function App () {
   return (
     <React.Fragment>
       <Header/>
-      <Body/>
+      <Router>
+        <React.Fragment>
+          <Route exact path="/" component={Body} />
+          <Route path="/test" component={Test} />
+        </React.Fragment>
+      </Router>
       <Footer/>
     </React.Fragment>
   );
--- webpack.dev.js
+++ webpack.dev.js
@@ -7,7 +7,8 @@ module.exports = merge(common, {
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist',
-    hot: true
+    hot: true,
+    historyApiFallback: true
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin()
